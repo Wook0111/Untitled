a1ca0e84fcfd6f2cbda74176ec64a9fd600c6854
Wook0111 <101158250+Wook0111@users.noreply.github.com>
2024-03-04 16:17:05 +0900

    Create Evaluation_IR.py

---
+++ Evaluation_IR.py
@@ -0,0 +1,26 @@
+import numpy as np
+from collections import OrderedDict
+
+class Evaluation_IR:
+    def __init__(self):
+        self.ks = ks
+        self.max_k = max(self.ks)
+
+    def evaluate(self, dataset, mode = 'valid'):
+        if mode == 'valid':
+            eval_mat = 
+            eval_id = 
+            qid2pid = 
+            doc_id = 
+        elif mode == 'test':
+            eval_mat = 
+            eval_id = 
+            qid2pid = 
+            doc_id = 
+
+        score_dict = OrderDict()
+        topns = self.ks
+        # make predictions and compute scores
+        
+
+  
