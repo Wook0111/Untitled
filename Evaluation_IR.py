e95a2d0cceb8f89c75272c197c5fb69e60edf59e
Wook0111 <101158250+Wook0111@users.noreply.github.com>
2024-03-04 18:30:56 +0900

    Update Evaluation_IR.py

--- Evaluation_IR.py
+++ Evaluation_IR.py
@@ -6,7 +6,7 @@ class Evaluation_IR:
         self.ks = ks
         self.max_k = max(self.ks)
 
-    def evaluate(self, dataset, mode = 'valid'):
+    def evaluate(self, model, dataset, mode = 'valid'):
         if mode == 'valid':
             eval_mat = 
             eval_id = 
@@ -21,6 +21,34 @@ class Evaluation_IR:
         score_dict = OrderDict()
         topns = self.ks
         # make predictions and compute scores
+
+        close_docs_topk = 
+
+
+def closest_docs(query, model, method, mode):
+    doc_length = 8841823
+    query_length = query_vectors.shape[0]
+    topk = 1000 
+    topk_docs = np.zeros((query_length, topk), dtype=np.int32)
+
+    if method == 'dot':
+        document_output = model.get_sparse_output(mode)
+    
+    return topk_docs
+
+def precision_at_k(actual, predicted, k):
+    return precision
+    
+def recall_at_k(actual, predicted, k):
+    return recall
+
+def ap_at_k(actual, predicted, k):
+    ap = sum(precision_at_i) / min(len(actual), k)
+    return ap
+
+def mrr(actual, predicted):
+    return mrr
+        
         
 
   
